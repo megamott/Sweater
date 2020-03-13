8db060f958f3a9ca719d19b45809571f002390c3
Matvey Konoplyov <megamott27@gmail.com>
2020-03-13 21:05:09 +0300

    Added User tag to messages

--- src/main/java/com/example/sweater/config/WebSecurityConfig.java
+++ src/main/java/com/example/sweater/config/WebSecurityConfig.java
@@ -1,6 +1,6 @@
 package com.example.sweater.config;
 
-import org.hibernate.internal.build.AllowSysOut;
+import com.example.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -19,6 +19,8 @@ import javax.sql.DataSource;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Autowired
     private DataSource dataSource;
+    @Autowired
+    private UserService userService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -37,11 +39,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource) // Ходить в базу данных для поиска пользователей и их ролей
-                .passwordEncoder(NoOpPasswordEncoder.getInstance()) // Шифрование паролей
-                .usersByUsernameQuery("select username, password, active from usr where username=?")
-                .authoritiesByUsernameQuery("select u.username, ur.roles from usr u inner join user_role ur on u.id = ur.user_id where u.username=?");
+        auth.userDetailsService(userService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance()); // Шифрование паролей
     }
 }
 
--- src/main/java/com/example/sweater/domain/Role.java
+++ src/main/java/com/example/sweater/domain/Role.java
@@ -1,8 +1,15 @@
 package com.example.sweater.domain;
 
+import org.springframework.security.core.GrantedAuthority;
+
 /**
  * @author Matvey
  */
-public enum Role {
+public enum Role implements GrantedAuthority {
     USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
