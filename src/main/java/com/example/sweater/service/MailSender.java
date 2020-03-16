ec09773952986769e0c466410b138cbb323520da
Matvey Konoplyov <megamott27@gmail.com>
2020-03-16 21:22:39 +0300

    Added email registration

---
+++ src/main/java/com/example/sweater/service/MailSender.java
@@ -0,0 +1,31 @@
+package com.example.sweater.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Matvey
+ */
+
+@Service
+public class MailSender {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    public void send(String emilTo, String subject, String message){
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+        simpleMailMessage.setFrom(username);
+        simpleMailMessage.setTo(emilTo);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(message);
+
+        mailSender.send(simpleMailMessage);
+    }
+}
